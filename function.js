2d134e4baa11ccb99f8d9da0d10c2ec8082ceb93
blwinkle <blwinkle@hotmail.com>
2017-02-14 08:22:02 -0600

    base site
    html, css and js

---
+++ function.js
@@ -0,0 +1,51 @@
+var lightbox = function(){
+    $('div.lbTrigger').click (
+      function() {
+	$('div.lbOverlay', this).slideToggle();
+          $('.lbContent img').toggleClass('pretty');
+      });
+    $('div.lbTrigger').click (
+      function(event) {
+	event.preventDefault();
+    
+      });
+  };
+
+/*var viewResume = function(){
+    $('div#nav').click(
+      function(){
+	$('#res').slideToggle('slow');
+      });
+   
+  };*/
+
+/*var viewResume = function(){
+    $('div#nav').click(
+      function(){
+	$('#res').toggleClass('block');
+          $('#res').toggleClass('group');
+          $('#res').toggleClass('hide');
+      });
+   
+  };*/
+
+var viewport = function(){
+    $('div.lbTrigger').hover(
+      function(){
+	$(this).toggleClass('imgHover');
+      });
+
+  };
+
+  $(document).ready(function(){
+    lightbox();
+//    viewResume();
+    viewport();
+      
+    $('#res').click(function(){
+        $('p', this).slideToggle('slow');
+    });
+      
+  });
+
+
